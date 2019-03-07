e5bb55a793235f6e1e46e7b56939885e09f5a882
Wamiq <wamiqurrehman093@gmail.com>
2019-03-07 15:00:36 +0500

    Walking and Running

---
+++ animate.py
@@ -0,0 +1,85 @@
+import arcade
+import os
+import math
+from fighter import *
+
+WIDTH = 800
+HEIGHT = 600
+
+SCALE = 0.5
+SPEED = 5
+
+BLUE = arcade.color.ALICE_BLUE
+
+LEFT = arcade.key.LEFT
+RIGHT = arcade.key.RIGHT
+
+class Window(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(path)
+
+        self.player = None
+
+    def on_draw(self):
+        arcade.start_render()
+        self.player.draw()
+
+    def setup(self):
+        arcade.set_background_color(BLUE)
+        self.player = Fighter()
+        self.player.stand_left = []
+        self.player.stand_right = []
+        self.player.walk_right = []
+        self.player.walk_left = []
+        self.player.run_right = []
+        self.player.run_left = []
+
+        self.player.stand_right.append(arcade.load_texture('images/stand/0.png', scale=SCALE))
+        self.player.stand_left.append(arcade.load_texture('images/stand/0.png', scale=SCALE, mirrored=True))
+
+        for i in range(10):
+            self.player.walk_right.append(arcade.load_texture('images/walk/' + str(i) + '.png', scale=SCALE))
+            self.player.walk_left.append(arcade.load_texture('images/walk/' + str(i) + '.png', scale=SCALE, mirrored=True))
+
+        for i in range(8):
+            self.player.run_right.append(arcade.load_texture('images/run/' + str(i) + '.png', scale=SCALE))
+            self.player.run_left.append(arcade.load_texture('images/run/' + str(i) + '.png', scale=SCALE, mirrored=True))
+
+        self.player.delta_distance = 20
+        self.player.center_x = WIDTH//2
+        self.player.center_y = HEIGHT//2
+        self.player.scale = SCALE
+
+    def update(self, delta_time):
+        self.player.update()
+        self.player.update_animation()
+
+    def on_key_press(self, key, mods):
+        if key == RIGHT:
+            self.player.change_x = SPEED
+        if key == LEFT:
+            self.player.change_x = -SPEED
+        if key == RIGHT and mods == arcade.key.MOD_SHIFT:
+            self.player.change_x = SPEED * 1.5
+            self.player.run = True
+        if key == LEFT and mods == arcade.key.MOD_SHIFT:
+            self.player.change_x = -SPEED * 1.5
+            self.player.run = True
+
+    def on_key_release(self, key, mods):
+        if key == RIGHT or key == LEFT:
+            self.player.change_x = 0
+            self.player.run = False
+        if mods == arcade.key.MOD_SHIFT:
+            self.player.run = False
+
+def main():
+    window = Window(WIDTH, HEIGHT)
+    window.setup()
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
---
+++ fighter.py
@@ -0,0 +1,141 @@
+import arcade
+import os
+import math
+
+FACE_RIGHT = 1
+FACE_LEFT = 2
+FACE_UP = 3
+FACE_DOWN = 4
+
+class Fighter(arcade.Sprite):
+    def __init__(self, scale: float=1,
+                 image_x: float=0, image_y: float=0,
+                 center_x: float=0, center_y: float=0):
+        super().__init__(scale=scale, image_x=image_x, image_y=image_y,
+                         center_x=center_x, center_y=center_y)
+        self.state = FACE_RIGHT
+        self.stand_right = None
+        self.stand_left = None
+        self.walk_left = None
+        self.walk_right = None
+        self.run_right = None
+        self.run_left = None
+        self.cur_index = 0
+        self.delta_distance = 20
+        self.prev_center_x = 0
+        self.prev_center_y = 0
+        self.run = False
+
+    def update_animation(self):
+        if self.run:
+            self.run_animation()
+        else:
+            self.walk_animation()
+
+    def run_animation(self):
+        """
+        Logic for selecting the proper texture to use.
+        """
+        x1 = self.center_x
+        x2 = self.prev_center_x
+        y1 = self.center_y
+        y2 = self.prev_center_y
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        texture_list = []
+
+        change_direction = False
+        if self.change_x > 0 \
+                and self.change_y == 0 \
+                and self.state != FACE_RIGHT \
+                and self.run_right \
+                and len(self.run_right) > 0:
+            self.state = FACE_RIGHT
+            change_direction = True
+        elif self.change_x < 0 and self.change_y == 0 and self.state != FACE_LEFT \
+                and self.run_left and len(self.run_left) > 0:
+            self.state = FACE_LEFT
+            change_direction = True
+
+        if self.change_x == 0 and self.change_y == 0:
+            if self.state == FACE_LEFT:
+                self._texture = self.stand_left[0]
+            elif self.state == FACE_RIGHT:
+                self._texture = self.stand_right[0]
+
+        elif change_direction or distance >= (self.delta_distance * 2.2):
+            self.prev_center_x = self.center_x
+            self.prev_center_y = self.center_y
+
+            if self.state == FACE_LEFT:
+                texture_list = self.run_left
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError("update_animation was called on a sprite that doesn't have a "
+                                       "list of walk left textures.")
+            elif self.state == FACE_RIGHT:
+                texture_list = self.run_right
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError("update_animation was called on a sprite that doesn't have a list of "
+                                       "walk right textures.")
+
+            self.cur_index += 1
+            if self.cur_index >= len(texture_list):
+                self.cur_index = 0
+
+            self._texture = texture_list[self.cur_index]
+
+        self.width = self._texture.width * self.scale
+        self.height = self._texture.height * self.scale
+
+    def walk_animation(self):
+        """
+        Logic for selecting the proper texture to use.
+        """
+        x1 = self.center_x
+        x2 = self.prev_center_x
+        y1 = self.center_y
+        y2 = self.prev_center_y
+        distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        texture_list = []
+
+        change_direction = False
+        if self.change_x > 0 \
+                and self.change_y == 0 \
+                and self.state != FACE_RIGHT \
+                and self.walk_right \
+                and len(self.walk_right) > 0:
+            self.state = FACE_RIGHT
+            change_direction = True
+        elif self.change_x < 0 and self.change_y == 0 and self.state != FACE_LEFT \
+                and self.walk_left and len(self.walk_left) > 0:
+            self.state = FACE_LEFT
+            change_direction = True
+
+        if self.change_x == 0 and self.change_y == 0:
+            if self.state == FACE_LEFT:
+                self._texture = self.stand_left[0]
+            elif self.state == FACE_RIGHT:
+                self._texture = self.stand_right[0]
+
+        elif change_direction or distance >= self.delta_distance:
+            self.prev_center_x = self.center_x
+            self.prev_center_y = self.center_y
+
+            if self.state == FACE_LEFT:
+                texture_list = self.walk_left
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError("update_animation was called on a sprite that doesn't have a "
+                                       "list of walk left textures.")
+            elif self.state == FACE_RIGHT:
+                texture_list = self.walk_right
+                if texture_list is None or len(texture_list) == 0:
+                    raise RuntimeError("update_animation was called on a sprite that doesn't have a list of "
+                                       "walk right textures.")
+
+            self.cur_index += 1
+            if self.cur_index >= len(texture_list):
+                self.cur_index = 0
+
+            self._texture = texture_list[self.cur_index]
+
+        self.width = self._texture.width * self.scale
+        self.height = self._texture.height * self.scale
